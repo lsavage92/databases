72dc81591fb64d702ff6c12ff3abeff17b59920e
Luke Savage <luke.savage92@gmail.com>
2015-07-24 18:31:49 -0500

    Created database schema

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+//Main file of server code
 var express = require('express');
 var db = require('./db');
 
--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,29 @@
+/*Defines structure of DB tables, file will be ran in CL to create tables*/
+
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE Users (
+  userId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(15)
 );
+
+CREATE TABLE Rooms (
+  roomId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  roomname VARCHAR(15)
+);
+
+CREATE TABLE Messages (
+  /* Describe your table here.*/
+  messageId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  messageText VARCHAR(140),
+  userId INT,
+  roomId INT,
+  FOREIGN KEY (userId) REFERENCES Users(userId),
+  FOREIGN KEY (roomId) REFERENCES Rooms(roomId)
+);
+
 
 /* Create other tables and define schemas for them here! */
 
