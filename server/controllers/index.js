2aba7000329d215e30a612a7a3a11bf022cd8d42
Luke Savage <luke.savage92@gmail.com>
2015-07-25 18:11:51 -0500

    Used models module to send request to db

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,18 +1,26 @@
 var models = require('../models');
 var bluebird = require('bluebird');
 
-
-
 module.exports = {
+// this is routed to 1337/classes/messages
   messages: {
     get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
-  },
+    post: function (req, res) {
 
+    } // a function which handles posting a message to the database
+  },
+// this is routed to 1337/classes/users
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) {
+      models.users.get(res);
+    },
+    post: function (req, res) {
+    }
+
+    /*options: function(req, res){
+    }*/
   }
 };
 
+//curl -H "Content-Type: application/json" -X POST -d '{"userName":"FRANK", "message": "hey my first message", "roomName": "anonomyous"}' http://127.0.0.1:1337/classes/messages
