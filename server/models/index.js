4f1b7057955b9d2834eef25e88c4ae0ecc877e2c
Luke Savage <luke.savage92@gmail.com>
2015-07-25 18:08:44 -0500

    Added users.GET query to get all users from the db

--- server/models/index.js
+++ server/models/index.js
@@ -6,13 +6,23 @@ var db = require('../db');
 module.exports = {
   messages: {
     get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    post: function (req) {
+
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (res) {
+      //db.connection.connect()
+      db.query('SELECT * FROM Users', function(err, result, fields) {
+        if (err) console.log(err);
+        console.log('Results: ', result);
+        res.send(result);
+      });
+    },
+    post: function () {
+
+    }
   }
 };
-
