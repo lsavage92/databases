12319455034a00aca80403f089ad766aa055ffd4
Luke Savage <luke.savage92@gmail.com>
2015-07-25 18:07:56 -0500

    Connected db

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,19 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var connection = mysql.createConnection({
+  user     : 'root',
+  password : 'HRR7mySQL92',
+  database : 'chat'
+});
 
+connection.connect(function(err) {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+}); // creating the connection
+
+module.exports = connection;
