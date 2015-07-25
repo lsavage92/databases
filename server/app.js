241841a0581a3ec630537dc8942668954116c66e
Luke Savage <luke.savage92@gmail.com>
2015-07-25 18:08:12 -0500

    Changed port to 1337

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ var app = express();
 module.exports.app = app;
 
 // Set what we are listening on.
-app.set("port", 3000);
+app.set("port", 1337);
 
 // Logging and parsing
 app.use(morgan('dev'));
